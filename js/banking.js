69ce9414f5119c646470b09f3b4adacb9a862da4
shagors <shajjadshagor@gmail.com>
2022-02-10 16:03:38 +0600

    deposit button ok and deposit amount show

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,12 @@
-console.log("i got it");
+document.getElementById("deposit-button").addEventListener("click", function(){
+    const depositInput = document.getElementById("deposit-input");
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // get current deposit
+    const depositTotal = document.getElementById("deposit-total");
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+    // clear input field
+    depositInput.value = "";
+});
